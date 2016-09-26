03df852e422687d7e13883987cbbd2996f5eb172
hh291029 <hh291029@C39444.ens.uqam.ca>
2016-09-26 10:26:16 -0400

    class1Hello

---
+++ src/demogit2/class1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demogit2;
+
+/**
+ *
+ * @author hh291029
+ */
+public class class1 {
+    public static void hello(){
+        System.out.println("Hello");
+    }
+    
+}
